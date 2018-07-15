041e7344f41a8c93492f04acc824bd81c58b4362
arimura <arimura@arimura.lan>
2018-07-16 07:22:37 +0900

    lecture3

---
+++ introduction-to-cpp/lecture3/pass-by-ref.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void increment(int &a) {
+  a = a + 1;
+  cout << "a in increment " << a << endl;
+}
+
+int main() {
+  int q = 3;
+  increment(q);
+  cout << "q in main " << q << endl;
+}
