d38df4f4be64d1e8d29da3a7eaf168b6d5353ce0
arimura <arimura@arimura.local>
2018-07-16 12:55:01 +0900

    implement monte carlro

--- introduction-to-cpp/problemset2/montecarlo.cpp
+++ introduction-to-cpp/problemset2/montecarlo.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 using namespace std;
 
 float randWithOne(){
-  return (rand() % 101) / (float)100;
+  return (rand() % 100) / (float)100;
 }
 
+int countDartWithinCircle(int tryCnt){
+  int cnt = 0;
+  for(int i=0; i < tryCnt; i++){
+    float x = randWithOne();
+    float y = randWithOne();
+    //cout << "x: " << x << ", y: " << y << endl;
+
+    //check dart is within circle
+    float d =  sqrt(pow(x,2) + pow(y,2));
+    //cout << "d: " << d << endl;
+
+    if(d <= 1) cnt++;
+  }
+  return cnt;
+}
 
 int main() {
   srand(time(0));
 
-  float x = randWithOne();
-  float y = randWithOne();
-  cout << "x: " << x << ", y: " << y << endl;
+  int tryCnt = 5000000;
+  int dartCnt = countDartWithinCircle(tryCnt);
+  cout << "try: " << tryCnt << ", inside: " << dartCnt << endl;
 
+  float pi = dartCnt / (float)tryCnt * 4.;
+  cout << "pi is " << pi;
   return 0;
 }
+
