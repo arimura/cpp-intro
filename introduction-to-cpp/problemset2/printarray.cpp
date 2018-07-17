760ca61330cb2ae9046523f71595a1fb5910a96d
arimura <arimura@arimura.lan>
2018-07-18 00:39:41 +0900

    reverse

--- introduction-to-cpp/problemset2/printarray.cpp
+++ introduction-to-cpp/problemset2/printarray.cpp
@@ -9,9 +9,18 @@ void printArray(int size, int array[]){
   }
 }
 
+void reverse(int size, int array[]){
+  for(int i = 0;i < size / 2; i++){
+    int tmp = array[i];
+    array[i] = array[(size-1-i)];
+    array[(size-1-i)] = tmp;
+  }
+}
+
 int main(){
-  int array[] = {1, 3, 5, 7};
+  int array[] = {1, 3, 5, 7, 9};
   int size = sizeof(array) / sizeof(*array);
+  reverse(size, array);
   printArray(size, array);
   return 0;
 }
